7170ef25f8ab23dd834b52c3144260726276be15
Lisandro Corbalan <lmcorbalan@gmail.com>
2019-09-05 13:03:56 -0300

    Create ExecutiveVotes and PollVotes

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,8 +1,9 @@
-import { log } from '@graphprotocol/graph-ts'
+import { log, BigInt } from '@graphprotocol/graph-ts'
 
 import { LinkConfirmed as LinkConfirmedEvent } from '../generated/VoteProxyFactory/VoteProxyFactory'
-import { LogNote } from '../generated/DSChief/DSChief'
-import { VoteProxy, VoterRegistry, GovernanceInfo, Action } from '../generated/schema'
+import { LogNote, DSChief, Etch } from '../generated/DSChief/DSChief'
+import { PollCreated } from '../generated/PollingEmitter/PollingEmitter'
+import { VoteProxy, VoterRegistry, GovernanceInfo, Action, ExecutiveVote, PollVote } from '../generated/schema'
 
 import { BIGDECIMAL_ZERO, BIGINT_ONE, BIGINT_ZERO, toBigDecimal } from './helpers'
 
@@ -96,4 +97,72 @@ function getLogNoteData(event: LogNote, method: string): VoteProxy {
     log.error('{}: VoteProxy with id {} not found.', [method, voteProxyId])
 
   return voteProxy as VoteProxy
+}
+
+export function handleEtch(event: Etch): void {
+  let executiveVote = new ExecutiveVote(
+    event.transaction.hash.toHex() + '-' + event.logIndex.toString(),
+  )
+
+  executiveVote.slate = event.params.slate
+
+
+  let contract = DSChief.bind(event.address)
+  // let response = contract.try_MAX_YAYS()
+  // let response = contract.try_slates(executiveVote.slate, BIGINT_ZERO)
+
+  // log.error('SLATE {}, address0 {}', [event.params.slate.toHexString(), response.value.toHexString()])
+
+  // log.debug(
+  //   'Reverted: {}, value: {}',
+  //   [
+    //     response.reverted ? 'YES' : 'NO',
+    //     response.value.toHexString()
+    //   ]
+    // )
+
+  executiveVote.save()
+
+  let action = new Action(
+    event.transaction.hash.toHex() + '-' + event.logIndex.toString(),
+  )
+  action.type = 'CRETATE_EXECUTIVE_VOTE'
+  action.sender = event.address // TODO - check this
+  action.transactionHash = event.transaction.hash
+  action.timestamp = event.block.timestamp
+  action.save()
+
+  let governanceInfo = getGovernanceInfoEntity()
+  governanceInfo.countExecutiveVotes = governanceInfo.countExecutiveVotes.plus(BIGINT_ONE)
+  governanceInfo.lastBlock = event.block.number
+  governanceInfo.save()
+}
+export function handlePollCreated(event: PollCreated): void {
+  let pollVote = new PollVote(
+    event.transaction.hash.toHex() + '-' + event.logIndex.toString(),
+  )
+
+  pollVote.creator = event.params.creator
+  pollVote.blockCreated = event.params.blockCreated
+  pollVote.pollId = event.params.pollId
+  pollVote.startDate = event.params.startDate
+  pollVote.endDate = event.params.endDate
+  pollVote.multiHash = event.params.multiHash
+  pollVote.url = event.params.url
+
+  pollVote.save()
+
+  let action = new Action(
+    event.transaction.hash.toHex() + '-' + event.logIndex.toString(),
+  )
+  action.type = 'CRETATE_POLL_VOTE'
+  action.sender = event.address // TODO - check this
+  action.transactionHash = event.transaction.hash
+  action.timestamp = event.block.timestamp
+  action.save()
+
+  let governanceInfo = getGovernanceInfoEntity()
+  governanceInfo.countPollVotes = governanceInfo.countPollVotes.plus(BIGINT_ONE)
+  governanceInfo.lastBlock = event.block.number
+  governanceInfo.save()
 }
